d98846da605f48953332ed49f24fa10b6e5baa3f
Bryan Müller <bmuller0975@gmail.com>
2019-11-18 00:43:36 -0700

    feat(Styles): Setup Typography with demo

---
+++ snui/src/components/Demo/Typography.vue
@@ -0,0 +1,28 @@
+<template>
+<section>
+  <p class="sn-display-1">Display 1 Aa</p>
+  <p class="sn-display-2">Display 2 Aa</p>
+  <p class="sn-display-3">Display 3 Aa</p>
+  <h1 class="sn-large-title">Large Title Aa</h1>
+  <h2 class="sn-title-1">Title 1 Aa</h2>
+  <h3 class="sn-title-2">Title 2 Aa</h3>
+  <h4 class="sn-title-3">Title 3 Aa</h4>
+  <h5 class="sn-headline">Headline Aa</h5>
+  <p class="sn-body">Body Aa</p>
+  <p class="sn-callout">Callout Aa</p>
+  <p class="sn-subhead">Subhead Aa</p>
+  <p class="sn-footnote">Footnote Aa</p>
+  <p class="sn-caption-1">Caption 1 Aa</p>
+  <p class="sn-caption-2">Caption 2 Aa</p>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Typography'
+}
+</script>
+
+<style scoped>
+
+</style>
