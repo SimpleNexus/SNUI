bad80eed2381000c20c1c8198ecbe4e98821e78f
Bryan Müller <bmuller0975@gmail.com>
2019-11-19 00:14:27 -0700

    feat(Storybook): Add spec export story
    Copies spec to public export folder

---
+++ snui/src/components/SpecExport/SpecExport.vue
@@ -0,0 +1,13 @@
+<template>
+<iframe src="export/index.html" width="100%" height="1000px"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'SpecExport'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ snui/src/components/SpecExport/specExport.stories.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue'
+
+import SpecExport from './SpecExport'
+
+storiesOf('SpecExport', module)
+  .add('Spec Export', () => ({
+    components: { SpecExport },
+    template: '<SpecExport />'
+  }))
