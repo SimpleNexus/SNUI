29fd656f6e79253e2158ce06d8b31d7f1448ef59
Bryan Müller <bmuller@simplenexus.com>
2020-03-05 10:46:52 -0700

    style(Button): Change validator param name

--- src/components/snui/SnButton/SnButton.vue
+++ src/components/snui/SnButton/SnButton.vue
@@ -49,8 +49,8 @@ export default {
       type: String,
       required: false,
       default: 'primary',
-      validator (size) {
-        return ['primary', 'secondary', 'caution', 'warning'].includes(size)
+      validator (type) {
+        return ['primary', 'secondary', 'caution', 'warning'].includes(type)
       }
     },
     /**
