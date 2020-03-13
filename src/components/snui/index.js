ad2ba451d04169cea6612fd57039a13877c59522
Bryan Müller <bmuller@simplenexus.com>
2020-03-13 16:00:39 -0600

    export suspense

--- src/components/snui/index.js
+++ src/components/snui/index.js
@@ -1,6 +1,7 @@
 import SnButton from './SnButton/SnButton'
 import SnTextField from './SnTextField/SnTextField'
 import SnAnimation from './SnAnimation/SnAnimation'
+import SnSuspense from './SnSuspense/SnSuspense'
 // import SnIcon from './SnIcon/SnIcon'
 // import SnOptionGroup from './SnOptionGroup/SnOptionGroup'
 // import SnAvatar from './SnAvatar/SnAvatar'
@@ -9,4 +10,4 @@ import SnAnimation from './SnAnimation/SnAnimation'
 // import SnAlert from './SnAlert/SnAlert'
 // import SnSwitch from './SnSwitch/SnSwitch'
 
-export { SnButton, SnTextField, SnAnimation }
+export { SnButton, SnTextField, SnAnimation, SnSuspense }
